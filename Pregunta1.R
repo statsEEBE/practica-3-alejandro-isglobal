a25fcb7ef5b69f4f1b23cf3dce4e90f6493e59cb
alejandro.caceres.dominguez <alejandro.caceres.dominguez@auleseebe.upc.edu>
2024-10-17 11:14:36 +0200

    ....

--- Pregunta1.R
+++ Pregunta1.R
@@ -1,3 +1,49 @@
+#Pregunta 1
+
+x <- c(0,1)
+f <-c(0.68, 0.32)
+
+plot(x, f, type="h", ylim=c(0,1), col="red")
+
+n <- 43
+Y <- function(i){sum(sample(x, n, f, replace=TRUE))}
+
+encuestas <- sapply(1:400000, Y)
+fr <- table(encuestas)/400000
+barplot(fr)
+fr["13"]
+
+dbinom(13, 43, 0.32)
+
+y<- 0:43
+plot(y,dbinom(y, 43, 0.32),type="h", col="red")
+
+dbinom(17, 44, 0.32)
+plot(y,dbinom(y, 44, 0.32),type="h", col="red")
+pbinom(16, 44, 0.32)
+#mediana 
+qbinom(0.5, 44, 0.32)
+qbinom(0.25, 44, 0.32)
+
+####
+
+24*0.68 #media
+24*0.68*0.32 #varianza
+qbinom(0.25, 24, 0.68)  #1er quartil
+
+y <- 0:43
+plot(y, pbinom(y, 24, 0.68), type="s")
+
+
+
+
+
+
+
+
+
+
+###########################################
 # ensayo de bernoulli
 
 x <- c(0,1)
